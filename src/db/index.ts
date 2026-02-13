d4519d7ba42f2c765fc3cf100e47258084a58c20
Emigdio Torres <emigdio821@gmail.com>
2026-02-12 20:04:50 -0600

    feat: add neon serverless support to drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,10 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
+import { neon } from '@neondatabase/serverless'
+import { drizzle as drizzleNeon } from 'drizzle-orm/neon-http'
+import { drizzle as drizzleNode } from 'drizzle-orm/node-postgres'
 import * as schema from '@/db/schema'
 import { env } from '@/lib/env'
 
-export const db = drizzle(env.DATABASE_URL, { schema })
+export const db =
+  env.DB_TYPE === 'neon'
+    ? drizzleNeon({ client: neon(env.DATABASE_URL), schema })
+    : drizzleNode(env.DATABASE_URL, { schema })
